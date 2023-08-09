c4576c369c8fa1e9c09b55b4ecc1d7be7640faf8
Martin Alejandro Luboszyc <103965308+AleLuboszyc@users.noreply.github.com>
2023-08-09 18:43:59 -0300

    Porfolio subiendo
    Porfolio parcialmente completado

---
+++ mi-cv-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar';
+import Home from './Pages/home/Home';
+import Acerca from './Pages/acerca/Acerca';
+import Portfolio from './Pages/portfolio/Portfolio';
+import Contacto from './Pages/contacto/Contacto';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='acerca' element={<Acerca />} />
+        <Route path='portafolio' element={<Portfolio />} />
+        <Route path='contacto' element={<Contacto />} />
+      </Routes>
+    </BrowserRouter>
+      
+  )
+}
+
+export default App
