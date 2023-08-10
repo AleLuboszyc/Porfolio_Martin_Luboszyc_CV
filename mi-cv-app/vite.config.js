5914fead5255213b8a22a79b4da4b4374e7ee11b
Martin Alejandro Luboszyc <103965308+AleLuboszyc@users.noreply.github.com>
2023-08-10 13:51:13 -0300

    Update vite.config.js

--- mi-cv-app/vite.config.js
+++ mi-cv-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://aleluboszyc.github.io/Porfolio_Martin_Luboszyc_CV/",
+  base:"/Porfolio_Martin_Luboszyc_CV/",
 })
